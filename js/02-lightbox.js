aa3b89ccc7082e41e95a2bd109a34a448f326b92
Oleksii Leschenko <djlabuh@gmail.com>
2023-03-06 21:57:48 +0200

    02 - lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+function createGalleryCardsMarkup(items) {
+  return items
+    .map(({ preview, original, description } = {}) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", cardsMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
